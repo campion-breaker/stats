0745c02ad8ca05edbbee9eb37bb5f20d4c1e9b15
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-24 21:45:13 -0500

    Moved the eventsList component under the events folder.

---
+++ src/components/events/EventsList.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Events from "./Events";
+
+export default function EventsList() {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Time
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Event
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Description
+                  </th>
+                </tr>
+              </thead>
+              <Events />
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
