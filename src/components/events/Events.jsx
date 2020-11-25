49f2e19fc72e4ed0e8a918b185d98a22df5e82b2
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-24 21:06:17 -0500

    Refactored class string for state display.

---
+++ src/components/events/Events.jsx
@@ -0,0 +1,58 @@
+import moment from "moment";
+import { useEffect } from "react";
+import { fetchEvents, selectAllEvents } from "./EventsSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function Events() {
+  const dispatch = useDispatch();
+  const eventStatus = useSelector((state) => state.events.status);
+  const events = useSelector(selectAllEvents);
+
+  useEffect(() => {
+    if (eventStatus === "idle") {
+      dispatch(fetchEvents());
+    }
+  }, [eventStatus, dispatch]);
+
+  let tableRow;
+
+  if (eventStatus === "done") {
+    tableRow = events.map((event) => {
+      return (
+        <tr key={event.TIME}>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="text-sm font-medium text-gray-900">
+              {moment(event.TIME).fromNow()}
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="text-sm text-gray-900">{event.NAME}</div>
+            <div className="text-sm text-gray-500">{event.ID}</div>
+          </td>
+          <td className="px-6 py-4">
+            <div className="text-sm text-gray-500">
+              {event.EVENT === "STATE_CHANGE"
+                ? "Circuit-Breaker Status Change"
+                : "Configuration Change"}
+            </div>
+          </td>
+          <td className="px-6 py-4 text-sm text-gray-500">
+            {event.EVENT === "STATE_CHANGE"
+              ? `Circuit-breaker flipped from ${event.OLD_STATE} to ${
+                  event.CIRCUIT_STATE
+                }. This was ${
+                  event.MODE === "AUTO" ? "an automatic" : "a manual"
+                } change.`
+              : `Circuit-breaker configuration settings were ${
+                  event.METHOD === "ADD" ? "added" : "changed"
+                }.`}
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  return (
+    <tbody className="bg-white divide-y divide-gray-200">{tableRow}</tbody>
+  );
+}
