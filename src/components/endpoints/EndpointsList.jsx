0b34dce577fd90a18a782b0338be0107a4f63981
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-24 21:48:16 -0500

    Changed from getting state from the events to getting the state from the configurations.

--- src/components/endpoints/EndpointsList.jsx
+++ src/components/endpoints/EndpointsList.jsx
@@ -1,33 +1,42 @@
 import { useEffect } from 'react';
-import { fetchEvents, selectAllEvents } from '../events/EventsSlice';
+import { fetchConfigurations } from '../configurations/configurationsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Endpoints from './Endpoints';
 
 export default function EndpointsList() {
   const dispatch = useDispatch();
-  const eventsStatus = useSelector((state) => state.events.status);
-  const events = useSelector((state) => {
-    const endpoints = state.events.endpoints;
-    return Object.keys(endpoints).map((endpoint) => endpoints[endpoint][0]);
+  const configurationsStatus = useSelector((state) => state.configurations.status);
+  const configurations = useSelector((state) => {
+    return state.configurations.ids.map((id) => state.configurations.entities[id]);
   });
 
   useEffect(() => {
-    if (eventsStatus === 'idle') {
-      dispatch(fetchEvents());
+    if (configurationsStatus === 'idle') {
+      dispatch(fetchConfigurations());
     }
-  }, [eventsStatus, dispatch]);
+  }, [configurationsStatus, dispatch]);
 
-  if (eventsStatus === 'done') {
+  if (configurationsStatus === 'done') {
+    console.log(configurations)
     return (
       <div>
         <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">
           Services Protected by Campion
         </h2>
 
-        <Endpoints items={events} />
+        <Endpoints items={configurations} />
       </div>
     );
   } else {
-    return null;
+    return (
+      <div className="flex justify-around mt-4">
+        <span className="relative inline-flex rounded-xl shadow-lg">
+          <span className="flex absolute h-5 w-5 top-0 right-0 -mt-1 -mr-1">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-5 w-5 bg-purple-500"></span>
+          </span>
+        </span>
+      </div>
+    )
   }
 }
