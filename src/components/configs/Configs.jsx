79f8f9246b8d0f36d56cf64ffa84e19de13f75b7
Ben Zelinski <bziggz@gmail.com>
2020-11-25 12:42:55 -0500

    New page for Individual Endpoint Configs

--- src/components/configs/Configs.jsx
+++ src/components/configs/Configs.jsx
@@ -1,3 +1,5 @@
+import { useLocation, Link } from "react-router-dom";
+
 export default function Configs({ items }) {
   const backgroundColors = [
     "bg-pink-600",
@@ -21,48 +23,53 @@ export default function Configs({ items }) {
 
     if (endpoint.CIRCUIT_STATE === "CLOSED") {
       displayedStateColor = "text-green-500";
-    } else if (endpoint.CIRCUIT_STATE === "OPEN" || endpoint.CIRCUIT_STATE === "FORCED-OPEN") {
+    } else if (
+      endpoint.CIRCUIT_STATE === "OPEN" ||
+      endpoint.CIRCUIT_STATE === "FORCED-OPEN"
+    ) {
       displayedStateColor = "text-red-500";
     } else if (endpoint.CIRCUIT_STATE === "HALF-OPEN") {
       displayedStateColor = "text-yellow-500";
     }
 
     return (
-      <li className="col-span-1 flex shadow-sm rounded-md">
-        <div
-          className={
-            "flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md " +
-            backgroundColors[i]
-          }
-        >
-          {initials}
-        </div>
-        <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-          <div className="flex-1 px-4 py-2 text-sm truncate">
-            <a
-              href="#"
-              className="text-gray-900 font-medium hover:text-gray-600"
-            >
-              {endpoint.NAME}
-            </a>
-            <p className={displayedStateColor}>{endpoint.CIRCUIT_STATE}</p>
+      <Link to={`/endpoints/${endpoint.ID}`}>
+        <li className="col-span-1 flex shadow-sm rounded-md">
+          <div
+            className={
+              "flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md " +
+              backgroundColors[i]
+            }
+          >
+            {initials}
           </div>
-          <div className="flex-shrink-0 pr-2">
-            <button className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <span className="sr-only">Open options</span>
-              <svg
-                className="w-5 h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
+          <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+            <div className="flex-1 px-4 py-2 text-sm truncate">
+              <a
+                href="#"
+                className="text-gray-900 font-medium hover:text-gray-600"
               >
-                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-              </svg>
-            </button>
+                {endpoint.NAME}
+              </a>
+              <p className={displayedStateColor}>{endpoint.CIRCUIT_STATE}</p>
+            </div>
+            <div className="flex-shrink-0 pr-2">
+              <button className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="sr-only">Open options</span>
+                <svg
+                  className="w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                </svg>
+              </button>
+            </div>
           </div>
-        </div>
-      </li>
+        </li>
+      </Link>
     );
   });
 
