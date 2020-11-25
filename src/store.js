dc3e427711eba59351f75dc527dc02b301b6c0d7
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-24 21:55:12 -0500

    Imported the configurationsSlice reducer and added to the configureStore.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import eventsReducer from './components/EventsSlice';
+import eventsReducer from './components/events/EventsSlice';
+import trafficReducer from './components/traffic/TrafficSlice';
+import configurationsReducer from './components/configurations/configurationsSlice';
 
 export default configureStore({
   reducer: {
     events: eventsReducer,
-  }
+    traffic: trafficReducer,
+    configurations: configurationsReducer,
+  },
 });
