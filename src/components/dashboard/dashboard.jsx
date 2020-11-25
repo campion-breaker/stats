faa5da636bcd69d0fbc4983ce01df280cc9b024a
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-24 21:58:36 -0500

    Created the dashboard component to clean up the App component and organize all components logically.

---
+++ src/components/dashboard/dashboard.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import TrafficList from "../traffic/TrafficList";
+import EndpointsList from "../endpoints/EndpointsList";
+import EventsList from "../events/EventsList";
+
+export default function Dashboard() {
+  return (
+    <React.Fragment>
+      <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+        <button className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <span className="sr-only">Open sidebar</span>
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+      <main
+        className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
+        tabindex="0"
+      >
+        <div className="py-6">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-2xl font-semibold text-gray-900">
+              Performance Snapshot
+            </h1>
+          </div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <div className="py-4">
+              <TrafficList />
+            </div>
+          </div>
+        </div>
+        <div className="py-6">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-2xl font-semibold text-gray-900">
+              Protected Endpoints
+            </h1>
+          </div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <div className="py-4">
+              <EndpointsList />
+            </div>
+          </div>
+        </div>
+        <div className="py-6">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-2xl font-semibold text-gray-900">
+              Activity Log
+            </h1>
+          </div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <div className="py-4">
+              <EventsList />
+            </div>
+          </div>
+        </div>
+      </main>
+    </React.Fragment>
+  )
+}
