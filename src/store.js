1fa8d25c75a8d847862f5442882601d4fa54bf4c
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-25 23:01:51 -0500

    Added the subdomain reducer.

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import eventsReducer from './components/events/EventsSlice';
 import trafficReducer from './components/traffic/TrafficSlice';
 import configurationsReducer from './components/configs/ConfigurationsSlice';
+import subdomainReducer from './components/subdomain/SubdomainSlice';
 
 export default configureStore({
   reducer: {
     events: eventsReducer,
     traffic: trafficReducer,
     configurations: configurationsReducer,
+    subdomain: subdomainReducer,
   },
 });
