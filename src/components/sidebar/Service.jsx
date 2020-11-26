18f2e9854c3f0352e3867505ccd5fd5f14f9e821
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-25 23:28:09 -0500

    Changed the sidebar icon.

--- src/components/sidebar/Service.jsx
+++ src/components/sidebar/Service.jsx
@@ -12,20 +12,8 @@ export default function Service({
       to={`/endpoints/?id=${endpointId}`}
       className={search === `?id=${endpointId}` ? active : inactive}
     >
-      <svg
-        className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-        />
+      <svg class="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
       </svg>
       {name}
     </Link>
