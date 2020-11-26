048912b583cecf175b562c27a87771ab643c561d
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-25 22:58:34 -0500

    Removed state from service.

--- src/components/sidebar/Service.jsx
+++ src/components/sidebar/Service.jsx
@@ -28,7 +28,7 @@ export default function Service({
           d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
         />
       </svg>
-      {name} {state}
+      {name}
     </Link>
   );
 }
