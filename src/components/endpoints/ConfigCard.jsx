e6150a82e639f4f0535c38e26ad890556e3e8253
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-25 22:55:04 -0500

    Refactored configCard. Reformated the card. Added a pill to display the circuit breaker state.

--- src/components/endpoints/ConfigCard.jsx
+++ src/components/endpoints/ConfigCard.jsx
@@ -7,15 +7,16 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../loader/Loader";
 
-TIMESPAN: 60;
-UPDATED_TIME: 1606339094664;
-
 export default function ConfigCard({ endpointId }) {
   const dispatch = useDispatch();
   const configStatus = useSelector((state) => state.configurations.status);
   const config = useSelector(
     (state) => state.configurations.entities[endpointId]
   );
+
+  function capitalize(str) {
+    return str[0] + str.slice(1).toLowerCase();
+  }
 
   useEffect(() => {
     if (configStatus === "idle") {
@@ -24,6 +25,17 @@ export default function ConfigCard({ endpointId }) {
   }, [configStatus, dispatch]);
 
   if (configStatus === "done") {
+    const state = config.CIRCUIT_STATE;
+    let pillColor;
+
+    if (state === 'CLOSED') {
+      pillColor = 'bg-green-500';
+    } else if (state === 'OPEN' || state === 'FORCED-OPEN') {
+      pillColor = 'bg-red-500';
+    } else if (state === 'HALF-OPEN') {
+      pillColor = 'bg-yellow-500';
+    }
+
     return (
       <div class="bg-white shadow overflow-hidden sm:rounded-lg">
         <div class="px-4 py-5 sm:px-6">
@@ -33,72 +45,90 @@ export default function ConfigCard({ endpointId }) {
           <p class="mt-1 max-w-2xl text-sm text-gray-500">{endpointId}</p>
         </div>
         <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
-          <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <dl class="grid grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-2">
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Current State</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {config.CIRCUIT_STATE}
-              </dd>
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">Current State</dt>
+                <dd class="mt-1 text-m text-gray-900 sm:mt-0 sm:col-span-2">
+                  <div class={"rounded-full h-6 w-1/3 mt-2 py-1 text-center font-semibold text-xs text-white " + pillColor}>{capitalize(state)}</div>
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">
-                Breaker State Last Updated
-              </dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {moment(config.UPDATED_TIME).fromNow()}
-              </dd>
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">
+                  Breaker State Last Updated
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {moment(config.UPDATED_TIME).fromNow()}
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Error Timeout</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {config.ERROR_TIMEOUT} sec
-              </dd>
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">Error Timeout</dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {config.ERROR_TIMEOUT} sec
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Max Latency</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">Max Latency</dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 {config.MAX_LATENCY} ms
-              </dd>
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">
                 Network Failure Threshold
-              </dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 {config.NETWORK_FAILURE_THRESHOLD}
-              </dd>
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">
                 Service Failure Threshold
-              </dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 {config.SERVICE_FAILURE_THRESHOLD}
-              </dd>
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">
-                Half-Open Request Percentage
-              </dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {config.PERCENT_OF_REQUESTS}%
-              </dd>
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">
+                  Half-Open Request Percentage
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {config.PERCENT_OF_REQUESTS}%
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">
-                # of Successes to Re-Close Circuit
-              </dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {config.SUCCESS_THRESHOLD}
-              </dd>
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">
+                  # of Successes to Re-Close Circuit
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {config.SUCCESS_THRESHOLD}
+                </dd>
+              </div>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">
-                Request Tracking Timespan
-              </dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {config.TIMESPAN} sec
-              </dd>
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">
+                  Request Tracking Timespan
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {config.TIMESPAN} sec
+                </dd>
+              </div>
             </div>
           </dl>
         </div>
